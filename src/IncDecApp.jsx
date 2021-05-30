ec618a12cb7eacf090b5a9d117f8b1f7cc5f05a1
Srijan-Bhardwaj <srijanbhardwaj52@gmail.com>
2021-05-30 12:17:19 +0530

    Updated code for ui

--- src/IncDecApp.jsx
+++ src/IncDecApp.jsx
@@ -3,10 +3,17 @@ import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Tooltip from "@material-ui/core/Tooltip";
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  icon:{
+    fontSize:'1.2em'
+  },
+});
 
 const App = () => {
   const [count, setCount] = useState(0);
-
+  const classes = useStyles();
   const incr = () => {
     setCount(count + 1);
   };
@@ -28,17 +35,17 @@ const App = () => {
           <div className="header"><span>{count}</span></div>
           <div className="btn">
             <Tooltip title="increment">
-              <IconButton
+              <IconButton 
                 color="primary"
                 className="btn1"
                 onClick={incr}
               >
-                <AddIcon />
+                <AddIcon className={classes.icon}/>
               </IconButton>
             </Tooltip>
             <Tooltip title="decrement">
               <IconButton color="secondary" className="btn2" onClick={decr}>
-                <DeleteIcon />
+                <DeleteIcon className={classes.icon}/>
               </IconButton>
             </Tooltip>
           </div>
